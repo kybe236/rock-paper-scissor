c7ac014d029ff2bd41e7dd5f6eb7367b2b43e8d2
kyrill belomenov <kyrill.belomenov@outlook.de>
2023-03-12 12:23:47 +0100

    added function for 3 game types and a match expression

--- main.py
+++ main.py
@@ -22,4 +22,24 @@ def mode():
         mode()
 
 
-mode()
+def mode_1():
+    pass
+
+
+def mode_2():
+    pass
+
+
+def mode_3():
+    pass
+
+
+game_mode = mode()
+
+match game_mode:
+    case 1:
+        mode_1()
+    case 2:
+        mode_2()
+    case 3:
+        mode_3()
