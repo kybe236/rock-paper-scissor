0edffd47e4a3479a6e11dd117abb3a3352c9636e
kyrill belomenov <kyrill.belomenov@outlook.de>
2023-03-12 16:21:26 +0100

    moved some functions to lib.py

---
+++ lib.py
@@ -0,0 +1,71 @@
+import sys
+import time
+import getpass
+import os
+
+
+def pick_compare(player1: int, player2: int):
+    match player1:
+        case 1:  # rock
+            match player2:
+                case 1:
+                    return 0
+                case 2:
+                    return 2
+                case 3:
+                    return 1
+        case 2:  # paper
+            match player2:
+                case 1:
+                    return 1
+                case 2:
+                    return 0
+                case 3:
+                    return 2
+        case 3:  # scissors
+            match player2:
+                case 1:
+                    return 2
+                case 2:
+                    return 1
+                case 3:
+                    return 0
+
+
+def user_pick(user: int):
+    print(f"1: rock{os.linesep}"
+          f"2: paper{os.linesep}"
+          f"3: scissor")
+
+    try:
+        x = int(getpass.getpass(f"player {user} : "))
+
+        if x not in (1, 2, 3):
+            print("error: number is not in range ", file=sys.stderr)
+            time.sleep(0.1)  # wait until stderr is printed (maybe a bug)
+            user_pick(user)
+
+        return x
+
+    except Exception as ex:
+        print(f"error: {ex}", file=sys.stderr)
+        user_pick(user)
+
+
+def mode():
+    try:
+        x = int(input(f"computer: 1{os.linesep}"
+                      f"multiplayer(2 player): 2{os.linesep}"
+                      f"online(not working): 3{os.linesep}"
+                      f"enter number: "))
+
+        if x not in (1, 2, 3):
+            print("error: number is not in range ", file=sys.stderr)
+            time.sleep(0.1)  # wait until stderr is printed (maybe a bug)
+            mode()
+
+        return x
+
+    except Exception as ex:
+        print(f"error: {ex}", file=sys.stderr)
+        mode()
--- main.py
+++ main.py
@@ -1,75 +1,5 @@
-import os
 import random
-import sys
-import time
-import getpass
-
-
-def mode():
-    try:
-        x = int(input(f"computer: 1{os.linesep}"
-                      f"multiplayer(2 player): 2{os.linesep}"
-                      f"online(not working): 3{os.linesep}"
-                      f"enter number: "))
-
-        if x not in (1, 2, 3):
-            print("error: number is not in range ", file=sys.stderr)
-            time.sleep(0.1)  # wait until stderr is printed (maybe a bug)
-            mode()
-
-        return x
-
-    except Exception as ex:
-        print(f"error: {ex}", file=sys.stderr)
-        mode()
-
-
-def user_pick(user: int):
-    print(f"1: rock{os.linesep}"
-          f"2: paper{os.linesep}"
-          f"3: scissor")
-
-    try:
-        x = int(getpass.getpass(f"player {user} : "))
-
-        if x not in (1, 2, 3):
-            print("error: number is not in range ", file=sys.stderr)
-            time.sleep(0.1)  # wait until stderr is printed (maybe a bug)
-            user_pick(user)
-
-        return x
-
-    except Exception as ex:
-        print(f"error: {ex}", file=sys.stderr)
-        user_pick(user)
-
-
-def pick_compare(player1: int, player2: int):
-    match player1:
-        case 1:  # rock
-            match player2:
-                case 1:
-                    return 0
-                case 2:
-                    return 2
-                case 3:
-                    return 1
-        case 2:  # paper
-            match player2:
-                case 1:
-                    return 1
-                case 2:
-                    return 0
-                case 3:
-                    return 2
-        case 3:  # scissors
-            match player2:
-                case 1:
-                    return 2
-                case 2:
-                    return 1
-                case 3:
-                    return 0
+import lib
 
 
 def computer():
@@ -77,8 +7,8 @@ def computer():
     wins_player_2 = 0
     while (wins_player_1 < 3) and (wins_player_2 < 3):
         pick_1 = random.randrange(1, 3)
-        pick_2 = user_pick(2)
-        x = pick_compare(pick_1, pick_2)
+        pick_2 = lib.user_pick(2)
+        x = lib.pick_compare(pick_1, pick_2)
 
         if x == 0:
             print("draw")
@@ -99,9 +29,9 @@ def multiplayer():
     wins_player_1 = 0
     wins_player_2 = 0
     while (wins_player_1 < 3) and (wins_player_2 < 3):
-        pick_1 = user_pick(1)
-        pick_2 = user_pick(2)
-        x = pick_compare(pick_1, pick_2)
+        pick_1 = lib.user_pick(1)
+        pick_2 = lib.user_pick(2)
+        x = lib.pick_compare(pick_1, pick_2)
 
         if x == 0:
             print("draw")
@@ -122,9 +52,9 @@ def online():
     wins_player_1 = 0
     wins_player_2 = 0
     while (wins_player_1 < 3) and (wins_player_2 < 3):
-        pick_1 = user_pick(1)
-        pick_2 = user_pick(2)
-        x = pick_compare(pick_1, pick_2)
+        pick_1 = lib.user_pick(1)
+        pick_2 = lib.user_pick(2)
+        x = lib.pick_compare(pick_1, pick_2)
 
         if x == 0:
             print("draw")
@@ -141,7 +71,7 @@ def online():
             print("player 2 wins")
 
 
-game_mode = mode()
+game_mode = lib.mode()
 
 match game_mode:
     case 1:
